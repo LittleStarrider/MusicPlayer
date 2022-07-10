c740193a15c5d110c9ef7cd1c28690c8e0e61e61
little_starrider <fabio.geisser@gmail.com>
2022-07-10 13:57:48 +0200

    funktionierender Player

--- src/Main.java
+++ src/Main.java
@@ -4,14 +4,21 @@ public class Main {
     public static void main(String[] args) {
         String filename = "C:/Users/fabio/Downloads/King Dedede_'s Theme.mp3";
         MusicPlayer mp3Player = new MusicPlayer(filename);
-        mp3Player.play();
+        mp3Player.start();
 
         Scanner sc = new Scanner(System.in);
 
         System.out.println("Write stop to stop the music: ");
 
-        if (sc.nextLine().equalsIgnoreCase("stop")) {
-            mp3Player.close();
-        }
+        String input;
+        do {
+            input = sc.nextLine();
+            if (input.equalsIgnoreCase("pause")) {
+                mp3Player.pause();
+            } else if (input.equalsIgnoreCase("resume")) {
+                mp3Player.resume();
+            }
+        } while (!input.equalsIgnoreCase("stop"));
+        mp3Player.stop();
     }
 }
--- src/MusicPlayer.java
+++ src/MusicPlayer.java
@@ -1,38 +1,108 @@
+import javazoom.jl.decoder.JavaLayerException;
 import javazoom.jl.player.Player;
+import javazoom.jl.player.advanced.AdvancedPlayer;
+import javazoom.jl.player.advanced.PlaybackEvent;
+import javazoom.jl.player.advanced.PlaybackListener;
 
 import java.io.BufferedInputStream;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class MusicPlayer {
-    private final String mp3FileToPlay;
-    private Player jlPlayer;
+    private String mp3FileToPlay = null;
+    private AdvancedPlayer player;
+    private FileInputStream fileInputStream;
+    private BufferedInputStream bufferedInputStream;
+    private int totalLength;
+    private int pauseLength = 0;
+    private Thread resumeThread;
+    private Thread playThread;
+
+    private Runnable runnablePlay = () -> {
+        try {
+            //code for play button
+            createPlayer(mp3FileToPlay);
+            totalLength = fileInputStream.available();
+            player.play();//starting music
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    };
+
+    private Runnable runnableResume = () -> {
+
+        try {
+            //code for resume button
+            createPlayer(mp3FileToPlay);
+            fileInputStream.skip(totalLength - pauseLength);
+            player.play();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    };
 
     public MusicPlayer(String mp3FileToPlay) {
         this.mp3FileToPlay = mp3FileToPlay;
     }
 
-    public void play() {
-        try {
-            FileInputStream fileInputStream = new FileInputStream(mp3FileToPlay);
-            BufferedInputStream bufferedInputStream = new BufferedInputStream(fileInputStream);
-            jlPlayer = new Player(bufferedInputStream);
-        } catch (Exception e) {
-            System.out.println("Problem playing mp3 file " + mp3FileToPlay);
-            System.out.println(e.getMessage());
-        }
-
-        new Thread() {
-            public void run() {
-                try {
-                    jlPlayer.play();
-                } catch (Exception e) {
-                    System.out.println(e.getMessage());
-                }
-            }
-        }.start();
+    public void start() {
+        //starting play thread
+        playThread = new Thread(runnablePlay);
+        playThread.start();
     }
 
-    public void close() {
-        if (jlPlayer != null) jlPlayer.close();
+    public void resume() {
+        //starting resume thread
+        if (mp3FileToPlay != null && resumeThread == null) {
+            resumeThread = new Thread(runnableResume);
+            resumeThread.start();
+        }
+    }
+
+    public void pause() {
+        //code for pause button
+        if (player != null && mp3FileToPlay != null) {
+            try {
+                pauseLength = fileInputStream.available();
+                player.close();
+                System.out.println("Playback paused...");
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+        }
+        if (resumeThread != null) {
+            resumeThread = null;
+        }
+    }
+
+    public void stop() {
+        //code for stop button
+        if (player != null) {
+            player.close();
+        }
+    }
+
+    private void createPlayer(String mp3FileToPlay) throws FileNotFoundException, JavaLayerException {
+        fileInputStream = new FileInputStream(mp3FileToPlay);
+        bufferedInputStream = new BufferedInputStream(fileInputStream);
+        player = new AdvancedPlayer(bufferedInputStream);
+        player.setPlayBackListener(new PlaybackListener() {
+            public void playbackStarted(PlaybackEvent playbackEvent)
+            {
+                System.out.println("Playback started..");
+                //		   thread.resume();
+            }
+
+            public void playbackFinished(PlaybackEvent playbackEvent)
+            {
+                System.out.println("Playback finished..");
+                try {
+                    pauseLength = fileInputStream.available();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
     }
 }
